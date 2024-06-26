c277751124c17726afe42051ae215ba39739e8b4
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-06-26 09:15:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,43 +2,44 @@ from flask import Flask, redirect,  url_for, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user
 
-# Configurations
+# CONFIGURATIONS
 app = Flask(__name__,  static_url_path='/static')
-app.secret_key = "hello"
+app.secret_key = "@ui561zX"
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://flask_with_auth_user:qJ1JI3SIIyqrS0CDSscK4Z3dAEbjot7h@dpg-cptlki1u0jms73eb67dg-a.singapore-postgres.render.com/flask_with_auth"
-app.config["SECRET_KEY"] = "abc"
+# LOCAL DATABASE: "sqlite:///db.sqlite"
+app.config["SECRET_KEY"] = "$d771yKt"
 db = SQLAlchemy()
 
-# Login Manager
+# LOGIN MANAGER
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-# Creating Database
+# DATABASE MODEL
 class Users(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(250), unique=True, nullable=False)
 	password = db.Column(db.String(250), nullable=False)
 
-# Initializing Database
+# INITIALIZING THE DATABASE
 db.init_app(app)
 
 with app.app_context():
 	db.create_all()
      
-# Load User
+# LOADING USER
 @login_manager.user_loader
 def loader_user(user_id):
 	return Users.query.get(user_id)
 
-# Routes
+# ROUTES
 @app.route("/")
 def home():
-	return redirect(url_for("dashboard"))
+    return redirect(url_for("dashboard"))
 
 @app.route("/dashboard")
 def dashboard():
-	if "user" in session:
-		user = session["user"]
+    if "user" in session:
+        user = session["user"]
         name = str(user).title()
         return render_template("dashboard.html", user=name)
     else:
@@ -46,18 +47,17 @@ def dashboard():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-	if request.method == "POST":
+    if request.method == "POST":
         user = Users.query.filter_by(username=request.form.get("username")).first()
-		# CHECK IF USER IS IN THE DATABASE
-		if user is None:
-        	return redirect(url_for("login"))
+        if user is None:
+             return redirect(url_for("login"))
         else:
-			if user.password == request.form.get("password"):
-	        	name = request.form.get("username")
-	        	session["user"] = name
-	        	login_user(user)
-	        	return redirect(url_for("dashboard"))
-	return render_template("login.html")
+            if user.password == request.form.get("password"):
+                name = request.form.get("username")
+                session["user"] = name
+                login_user(user)
+                return redirect(url_for("dashboard"))
+    return render_template("login.html")
 
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
